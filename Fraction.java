60c8f5324233323f3334e537c22e17f49b8dcb07
Crfrocks <71032313+Crfrocks@users.noreply.github.com>
2020-10-19 15:03:33 -0400

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,40 +1,40 @@
 import java.lang.Math;
 public class Fraction {
-	public static int sharkie() {
-		FractionTester numerator1 = new FractionTester();
-		int x1 = FractionTester.sharks(); 
-		return x1;
+	
+	private int num;
+	private int denom;
+	
+	public Fraction(Fraction other) {
+		num = other.num;
+		denom = other.denom;
 	}
-
-	public static int skarkie2() {
-		FractionTester denominator1 = new FractionTester();
-		int y1 = FractionTester.sharks1();
-		return y1;
+	public Fraction(int n, int d) {
+		num = n;
+		denom = d; 
 	}
-	public static int sharkie3() {
-		FractionTester numerator2 = new FractionTester();
-		int x2 = FractionTester.sharks2();
-		return x2;
+	public Fraction(int i) {
+	}
+	public Fraction multiply (Fraction other) {
+		Fraction result = new Fraction (num * other.num , denom * other.denom);
+		
+		return result;
+	}
+	public String toString() {
+		return "" + num + "/" +  denom;
+	}
+	Fraction divide (Fraction other) {
+		new Fraction(num * other.denom, denom * other.num);
+		Fraction result2 = new Fraction(num * other.denom, denom * other.num );
+		return result2;
+	}
+	public String toString1() {
+		return "" + num + "/" + denom;
+	
 	}
 	
-	public static int sharkie4() {
-		FractionTester denominator2 = new FractionTester();
-		int y2 = FractionTester.sharks3();
-		return y2;
-	}
-	public static int nummult() {
-		int n1 = Fraction.sharkie();
-		int n2 = Fraction.sharkie3();
-		int numtimes = n1 * n2;
-		return numtimes;
-	}
-	public static int denmult() {
-		int d1 = Fraction.skarkie2();
-		int d2 = Fraction.sharkie4();
-		int dentimes = d1 * d2;
-		return dentimes;
 }
-}
+
+	
 			
 		
 
