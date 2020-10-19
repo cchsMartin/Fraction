de744e8b22e1edd0c23e9b441509253ebd4d8bd2
Crfrocks <71032313+Crfrocks@users.noreply.github.com>
2020-10-19 15:03:59 -0400

    Update FractionTester.java

--- FractionTester.java
+++ FractionTester.java
@@ -1,39 +1,16 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class FractionTester {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please state your fraction with a space inbetween");
-		int num1 = scan.nextInt();
-		int num2 = scan.nextInt();
-		int den1 = scan.nextInt();
-		int den2 = scan.nextInt();
+		Fraction a = new Fraction(1, 2);
+		Fraction b = new Fraction(2, 3);
 		
-	}
-	public static int sharks() {
-		Scanner scanner = new Scanner(System.in);
-		int num1 = scanner.nextInt();
-		return num1;
-	}
-	public static int sharks1() {
-		Scanner scanner = new Scanner(System.in);
-		int den1 = scanner.nextInt();
-		return den1;
-
-
-}
-	public static int sharks2(){
-		Scanner scanner = new Scanner(System.in);
-		int num2 = scanner.nextInt();
-		return num2;
-	}
-	public static int sharks3() {
-		Scanner scanner = new Scanner(System.in);
-		int den2 = scanner.nextInt();
-		return den2;
-	}
-	public static void main1(String[] args) {
-		System.out.println(Fraction.denmult());
+		Fraction c = a.multiply(b);
+		Fraction d = a.divide(b);
+		System.out.println(a + " * " + b + " = " + c);
+		System.out.println(a + " / " + b + " = " + c);
 	}
 }
+
 
